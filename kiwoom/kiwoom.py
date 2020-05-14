8e64e7046ce9431f0a1b2f9f14281a90f74074b9
Jace-JaehoonKim <hoon@Jaehoons-MacBook-Pro.local>
2020-05-14 11:53:21 +0900

    kiwoom test

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,3 +1,9 @@
-class Kiwoom():
+from PyQt5.QaxContainer import *  # p98
+
+class Kiwoom(QAxWidget):
     def __init__(self):
+        super().__init__() #상속받은 클래스의 init을 실행시키는 명령어
         print('kiwoom클래스입니다')
+
+
+    def get_ocx_instance

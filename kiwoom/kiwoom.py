782a60df6121a9ce2beabb3682a18f9f271f38e5
Jace-JaehoonKim <hoon@Jaehoons-MacBook-Pro.local>
2020-05-14 12:46:41 +0900

    37강

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,9 +1,36 @@
 from PyQt5.QaxContainer import *  # p98
+from PyQt5.QtCore import *  # 핸들값 다루기 위한 이벤트루프 클래스
 
 class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__() #상속받은 클래스의 init을 실행시키는 명령어
+        
         print('kiwoom클래스입니다')
 
+        ##############evnetloop모음##############
+        self.login_event_loop = None
+        ########################################
 
-    def get_ocx_instance
+        self.get_ocx_instance()
+        self.event_slots
+
+        self.signal_login_commConnect
+
+
+
+    def get_ocx_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot) 
+
+    def login_slot(self, errCode):
+        print(errCode)
+
+        self.login_event_loop.exit()
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")  #pyqt 모듈
+
+        self.login_event_loop=QEventLoop()
+        self.login_event_loop.exec_()  # 
--- ui/ui.py
+++ ui/ui.py
@@ -1,7 +1,13 @@
 from kiwoom.kiwoom import *
+from PyQt5.Qtwidgets import *
+import sys
 
 class Ui_class():
     def __init__(self):
         print('ui class입니다')
 
-        Kiwoom()
+        self.app=QApplication(sys.argv) #36강_  리스트 형태로 담겨져 있는  sys.argv = ['파이썬파일경로','옵션',...]
+
+        self.kiwoom=Kiwoom()
+
+        self.app.exec_() #36강_ 이벤트 루프를 실행시켜주는 명령어 계속 이벤트루프랑 돌도록
